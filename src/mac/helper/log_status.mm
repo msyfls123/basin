b88d33e155bd6b28128b933c51322d88f2f53e6a
msyfls123 <msyfls123@gmail.com>
2021-11-05 23:58:16 +0800

    chore: format code.

--- src/mac/helper/log_status.mm
+++ src/mac/helper/log_status.mm
@@ -2,16 +2,16 @@
 
 void log_status(napi_status status, napi_env env)
 {
-  if (status != napi_ok) {                                      
-    const napi_extended_error_info* error_info = NULL;          
-    napi_get_last_error_info((env), &error_info);               
-    bool is_pending;                                            
-    napi_is_exception_pending((env), &is_pending);              
-    if (!is_pending) {                                          
-        const char* message = (error_info->error_message == NULL) 
-            ? "empty error message"                               
-            : error_info->error_message;                          
-        NSLog(@"LOG_STATUS: %@", [NSString stringWithUTF8String:message]);                                              \
-    }                                                           
+  if (status != napi_ok) {
+    const napi_extended_error_info *error_info = NULL;
+    napi_get_last_error_info((env), &error_info);
+    bool is_pending;
+    napi_is_exception_pending((env), &is_pending);
+    if (!is_pending) {
+      const char *message = (error_info->error_message == NULL)
+                                ? "empty error message"
+                                : error_info->error_message;
+      NSLog(@"LOG_STATUS: %@", [NSString stringWithUTF8String:message]);
+    }
   }
 }
